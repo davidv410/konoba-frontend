b45a569ad787b1695a11a619b6c43238491122f8
davidv410 <davidvrgoc99@gmail.com>
2025-04-02 11:23:21 +0200

    update basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import Article from './components/Article/Article'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const router = createBrowserRouter([
-  {path: "/", element: <App/>},
-  {path: "/article/:title", element: <Article/>}
+  [
+    { path: "/", element: <App /> },
+    { path: "/article/:title", element: <Article /> }
+  ],
+  {
+    basename: "/konoba-frontend",
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
