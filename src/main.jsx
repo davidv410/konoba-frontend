bff3467482390f9e6bdf4c2fc7bf36502d4db785
davidv410 <davidvrgoc99@gmail.com>
2025-05-28 13:18:55 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
 import Article from './components/Article/Article'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createHashRouter, RouterProvider } from "react-router-dom"
 import BookConfirm from './components/bookatable/bookConfirm/BookConfirm'
 
 const router = createBrowserRouter(
