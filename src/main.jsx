24e789bf81a5b6b46f792d7c6d82b0a919abaf17
davidv410 <davidvrgoc99@gmail.com>
2025-06-25 16:43:02 +0200

    css fix

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,10 @@ const router = createBrowserRouter(
     { path: "admin-login", element: <Admin />},
     { path: "admin-confirm", element: <BookConfirm />}
   ]
+  ,
+  {
+    basename: "/konoba-frontend",  // kad pusham ovo sve obrisat
+  }
 )
 
 createRoot(document.getElementById('root')).render(
